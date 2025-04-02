37fcf4a9f5f7a940e01162bf4485e59e07a7d555
SanctumSpyre <axe33333@outlook.com>
2025-04-02 09:59:33 -0400

    saving work. code is broken as of now working to add masters pui to game generation

--- main.py
+++ main.py
@@ -64,7 +64,6 @@ def find_player_data(match_data, puuid):
 def get_match_data(puuid, match_ids, region):
     keys = ['champion', 'kills', 'deaths', 'assists', 'win']
     data = { key: [] for key in keys }
-
     for id in match_ids:
         raw_data = get_match_raw(id, region)
         player_data = find_player_data(raw_data, puuid)
@@ -94,17 +93,47 @@ def update_db(keys, data, db):
         
     conn.commit()
 
+def masters_puuids_retrieval(region, count):
+    queue = "RANKED_SOLO_5x5"
+    if region == 'americas':
+        region = 'na1'
+    url = (
+        "https://" +
+        region +
+        ".api.riotgames.com/lol/league/v4/masterleagues/by-queue/" + 
+        queue
+    )
+    api_url = make_url(url)
+    print(api_url)
+    print("https://na1.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-2711f6ce-843f-4a6f-8317-0fa47b9f070b")
+    resp = requests.get(api_url)
+    masters_player_data = resp.json()
+    list_of_masters_puuids = []
+    i = 0
+    while i < count:
+        if (masters_player_data["entries"][i]["inactive"]) == True:
+            i += 1
+            count += 1
+            continue
+        else:
+            list_of_masters_puuids.append(masters_player_data["entries"][i]["puuid"])
+            i += 1
+    return list_of_masters_puuids
 
 if __name__ == '__main__':
     mydb = 'stats.db'
     habib4d_puuid = '8XG2EdVepNrwc4w5_BnvPWjoGsdULwNIRFKrzoBBI0oskwMlrRzHD6t4vMCZe-tKyPUVlj5_eMR8eQ'
     SussyBaka2_puuid = "pntMCPZS4W3MPpYWYyHTkzqrod8CzRl7Cxt1QhULmyzaS_S7EmIaI19ngDu1v2NThAZhfjpTllPkEg"
-    puuid = SussyBaka2_puuid #change this to habib to work with yours
+    #puuid = SussyBaka2_puuid #change this to habib to work with yours
     region = 'americas'
-    match_ids = get_match_ids(puuid, region, 50)
+    masters_puuids = masters_puuids_retrieval(region, 30)
+    for puuid in masters_puuids:
+            match_ids = get_match_ids(puuid, region, 5)
 
-    keys, data = get_match_data(puuid, match_ids, region)
-    update_db(keys, data, mydb)
+            keys, data = get_match_data(puuid, match_ids, region)
+            update_db(keys, data, mydb)
+
+    #masters_puuids_retrieval(region, 30)
 
     conn = sqlite3.connect(mydb)
     cursor = conn.cursor()
