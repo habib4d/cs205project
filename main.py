7c9abf388c184539b250693b8b7e42f5d5cc3b52
SanctumSpyre <axe33333@outlook.com>
2025-04-04 11:33:47 -0400

    adding match ids and dates to database

--- main.py
+++ main.py
@@ -141,12 +141,18 @@ def summoners_in_league(queue, tier, division):
 
 
 if __name__ == '__main__':
-    mydb = 'stats.db'
-    habib4d_puuid = '8XG2EdVepNrwc4w5_BnvPWjoGsdULwNIRFKrzoBBI0oskwMlrRzHD6t4vMCZe-tKyPUVlj5_eMR8eQ'
-    SussyBaka2_puuid = "pntMCPZS4W3MPpYWYyHTkzqrod8CzRl7Cxt1QhULmyzaS_S7EmIaI19ngDu1v2NThAZhfjpTllPkEg"
-    #puuid = SussyBaka2_puuid #change this to habib to work with yours
-    region = 'americas'
 
+    mydb = 'stats.db'
+    conn = sqlite3.connect(mydb)
+    cursor = conn.cursor()
+
+
+    ''' habib4d_puuid = '8XG2EdVepNrwc4w5_BnvPWjoGsdULwNIRFKrzoBBI0oskwMlrRzHD6t4vMCZe-tKyPUVlj5_eMR8eQ'
+        SussyBaka2_puuid = "pntMCPZS4W3MPpYWYyHTkzqrod8CzRl7Cxt1QhULmyzaS_S7EmIaI19ngDu1v2NThAZhfjpTllPkEg" '''
+  
+    region = 'americas'
+    server = 'NA1' 
+    
     # retrieves all puuids of a certain rank
     masters_puuids = summoners_in_league('RANKED_SOLO_5x5', 'MASTER', "I")
     master_match_ids = []
@@ -156,12 +162,27 @@ if __name__ == '__main__':
         match_id = get_match_ids(puuid, region, 100)
         master_match_ids.append(match_id)
         time.sleep(120)
-    print(master_match_ids)
-    conn = sqlite3.connect(mydb)
-    cursor = conn.cursor()
+
+    unique_match_ids = set()
+
+    for lst in master_match_ids:
+        for match_id in lst:
+            unique_match_ids.add(match_id)
+
+    for match_id in unique_match_ids:
+        unix_date = get_match_raw(match_id, region)['info']['gameCreation']
+
+        try:
+            cursor.execute('insert into matches values (?, ?)',(match_id,unix_date))
+        except sqlite3.Error as er:
+            conn.close()
+            f'SQL error: {er}'
+
+        time.sleep(1.2)
+
 
     try:
-        cursor.execute(' select * from player_game_stats; ')
+        cursor.execute(' select * from matches ')
     except sqlite3.Error as er:
         conn.close()
         f'SQL error: {er}' 
